d300558949aeee98690cffacf71420eec862f386
Akash <akash51804@gmail.com>
2022-02-08 15:38:34 -0500

    added URL

--- src/test/java/com/lmhscodingclub/newscompass/AppTest.java
+++ src/test/java/com/lmhscodingclub/newscompass/AppTest.java
@@ -1,8 +1,10 @@
 package com.lmhscodingclub.newscompass;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.*;
+import java.io.*;
+import java.net.*;
 
 /**
  * Unit test for simple App.
@@ -14,5 +16,26 @@ class AppTest {
     @Test
     void testApp() {
         assertEquals(1, 1);
+    }
+
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=lWfWKkSRD6wWl9PvgxjkZ5bs2KYtLBMF");
+        URLConnection url_open = url.openConnection();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(url_open.getInputStream()));
+
+        String link;
+        while ((link = br.readLine()) != null) {
+            System.out.println(link);
+        }
+        br.close();
+
+        System.out.println("Done");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
