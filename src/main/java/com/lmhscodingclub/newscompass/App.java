2133acc10c9dff6089d4607858b823f5b428807c
Varun <31084854+varunsingh87@users.noreply.github.com>
2022-02-15 15:32:11 -0500

    Add text field for search bar (not styled)

--- src/main/java/com/lmhscodingclub/newscompass/App.java
+++ src/main/java/com/lmhscodingclub/newscompass/App.java
@@ -1,6 +1,9 @@
 package com.lmhscodingclub.newscompass;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import java.awt.FlowLayout;
 
 /**
  * Hello world!
@@ -11,9 +14,15 @@ public final class App extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
         setTitle("NewsCompass");
+
+        JTextField textField = new JTextField("Search news");
+        textField.setBounds(10, 10, 100, 20);
+
+        add(textField);
     }
 
     public static void main(String[] args) {
+        new App();
         NewsRequest r = new NewsRequest("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=lWfWKkSRD6wWl9PvgxjkZ5bs2KYtLBMF");
         NewsSource s = new NewsSource(PoliticalBias.LEFT, "The New York Times");
         NewsArticle a = new NewsArticle("The New York Times", "https://www.nytimes.com/2019/04/10/us/politics/trump-kim-north-korea-summit.html", s);
